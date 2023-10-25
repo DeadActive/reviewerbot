09dede127a29a4fdf00038259702381546c80af3
deadactive <undead.active@gmail.com>
2023-10-25 21:37:20 +0600

    upd karma

--- app.ts
+++ app.ts
@@ -4,16 +4,59 @@ import { Telegraf } from 'telegraf'
 import { InlineQueryResult, InlineQueryResultArticle, InputMessageContent } from 'telegraf/typings/core/types/typegram'
 config()
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
-const names: Array<string> = ['deadactive', 'NikitaKozlovR', 'newfox79', 'vldmrmatveev']
+interface IName {
+    name: string
+    weight: number
+}
 
-function pick(array: Array<string>, exclude: string) {
-    const fArray = array.filter(name => name !== exclude)
-    return `@${fArray[Math.floor(Math.random() * fArray.length)]}`
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const names: Array<IName> = [
+    {
+        name: 'deadactive',
+        weight: 1,
+    },
+    {
+        name: 'NikitaKozlovR',
+        weight: 1,
+    },
+    {
+        name: 'newfox79',
+        weight: 1,
+    },
+    {
+        name: 'vldmrmatveev',
+        weight: 1,
+    },
+]
+
+function choice(items: Array<IName>, exclude: string) {
+    const fItems = items.filter(item => item.name !== exclude)
+
+    const cumulativeWeights = []
+
+    fItems.forEach((name, i) => {
+        cumulativeWeights[i] = name.weight + (cumulativeWeights[i - 1] || 0)
+    })
+
+    const maxWeight = cumulativeWeights[cumulativeWeights.length - 1]
+    const random = maxWeight * Math.random()
+
+    for (let i = 0; i < fItems.length; i += 1) {
+        if (cumulativeWeights[i] >= random) {
+            const result = fItems[i]
+            result.weight /= 2
+            items.forEach(item => {
+                if (item.name !== result.name) {
+                    item.weight *= item.weight < 1 ? 2 : 1
+                }
+            })
+            return result.name
+        }
+    }
 }
 
 bot.on('inline_query', async ctx => {
-    const reviewer = pick(names, ctx.from.username)
+    const reviewer = choice(names, ctx.from.username)
     const result: InlineQueryResultArticle = {
         type: 'article',
         id: randomUUID(),
